967d2ab76d6509d763af12197de72aabd5d21d6e
Mihail Volkov <iMisha.Volkov@gmail.com>
2022-11-29 10:53:30 +0300

    обновление экрана рекордов

--- MyMinesWeeper/RecordsScreen/Controller/RecordsTableViewController.swift
+++ MyMinesWeeper/RecordsScreen/Controller/RecordsTableViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class RecordsTableViewController: UITableViewController {
     let cellIdentifier = "recordCell"
-
-    // 0-easy/1-medium/2-hard
-    var records = [[Record](), [Record](), [Record]()] {
-        didSet {
-            for (i, item)  in records.enumerated() {
-                records[i] = item.sorted { $0.time < $1.time }
-            }
-        }
-    }
+    
+    var resetButtons: [UIButton]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Рекорды"
-        records = [
+        resetButtons = Array(repeating: createResetButton(), count: RecordType.allCases.count)
+// testing
+        RecordsManager.shared.records = [
             [Record(time: 1234, nickName: "Foo", type: .easy),
              Record(time: 20000, nickName: "Fooooo1", type: .easy),
              Record(time: 4000, nickName: "Foo2", type: .easy),
@@ -40,7 +35,42 @@ class RecordsTableViewController: UITableViewController {
              Record(time: 3000, nickName: "Foo2", type: .hard)
             ]
         ]
+   
+    }
+    
+    private func setupResetButton(forSection section: Int) {
+        resetButtons[section] = createResetButton()
         
+        if let type = RecordType(rawValue: section) {
+            if (RecordsManager.shared.getRecords(forType: type)).isEmpty {
+                resetButtons[section].isEnabled = false
+            }
+        }
+        
+        let action = UIAction { [weak self] _ in
+            if let type = RecordType(rawValue: section) {
+                RecordsManager.shared.resetRecords(forType: type)
+                self?.resetButtons[section].isEnabled = false
+                self?.tableView.reloadSections(IndexSet(integer: section), with: .fade)
+            }
+        }
+        resetButtons[section].addAction(action, for: .touchUpInside)
+    }
+    
+    private func createResetButton() -> UIButton {
+        let button = UIButton(configuration: .plain())
+        button.frame = CGRect(x: tableView.bounds.width / 2 - 100, y: 0,
+                              width: 200, height: 30)
+        
+        let title = "Сбросить рекорды"
+        let attrEnabled = [NSAttributedString.Key.foregroundColor: UIColor.red,
+                           NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 17)]
+        let attrDisabled = [NSAttributedString.Key.foregroundColor: UIColor.gray,
+                            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)]
+        button.setAttributedTitle(NSAttributedString(string: title, attributes: attrEnabled), for: .normal)
+        button.setAttributedTitle(NSAttributedString(string: title, attributes: attrDisabled), for: .disabled)
+        
+        return button
     }
 
     // MARK: - Table view data source
@@ -50,18 +80,24 @@ class RecordsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let section = RecordType(rawValue: section) else { fatalError() }
+        guard let section = RecordType(rawValue: section) else { return 0 }
+        let records = RecordsManager.shared.getRecords(forType: section)
         
-        switch section {
-            case .easy: return records[0].count
-            case .medium: return records[1].count
-            case .hard: return records[2].count
-        }
+        return records.count
     }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
 
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        guard let section = RecordType(rawValue: section) else { fatalError() }
+        setupResetButton(forSection: section)
         
+        let view = UIView()
+        view.addSubview(resetButtons[section])
+        
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let section = RecordType(rawValue: section) else { return nil }
         var text = " сложность:"
         switch section {
             case .easy: text = "Лёгкая" + text
@@ -72,13 +108,14 @@ class RecordsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? RecordCell else { fatalError("Record cell not created") }
-        guard let recordType = RecordType(rawValue: indexPath.section) else { fatalError("RecordType not created") }
-
-        switch recordType {
-            case .easy: cell.record = records[0][indexPath.row]
-            case .medium: cell.record = records[1][indexPath.row]
-            case .hard: cell.record = records[2][indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? RecordCell else {
+            print("Record cell not created")
+            return UITableViewCell()
+        }
+        
+        if let recordType = RecordType(rawValue: indexPath.section) {
+            let records = RecordsManager.shared.getRecords(forType: recordType)
+            cell.record = records[indexPath.row]
         }
         return cell
     }
--- MyMinesWeeper/SupportingFiles/Constants.swift
+++ MyMinesWeeper/SupportingFiles/Constants.swift
@@ -12,5 +12,6 @@ final class Constants {
     static let minBombsCount = 10
     static let minFieldSize = 10
     static let maxFieldSize = 100
+    static let countRecordsForEachSection = 7
     
 }
